915d3da5a1dc066e31290dc4537578a663ab69c0
Lee Ar U. Ramirez <leear.ramirez@msunaawan.edu.ph>
2025-11-21 16:59:12 +0800

    (video 3) Get properties in the frontend and list them  / Lee Ar U. Ramirez

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,13 +1,49 @@
-import PropertyListItem from "./PropertyListItem";
+'use client';
 
+import { useEffect, useState } from 'react';
+import PropertyListItem from './PropertyListItem';
+
+
+export type PropertyType = {
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+}
 
 const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+    const getProperties = async () => {
+        try {
+            const url = 'http://127.0.0.1:8000/api/properties/';
+            const response = await fetch(url, {
+                method: 'GET',
+            });
+            const json = await response.json();
+            setProperties(json.data);
+        } catch (error) {
+            console.log('error', error);
+        }
+    };
+
+    useEffect(() => {
+        getProperties();
+    }, []);
+
     return (
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+            {properties.map((property) => {
+                return (
+                    <PropertyListItem
+                        key={property.id}
+                        property={property}
+                    // markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
+                    />
+                )
+            })}
         </>
     )
 }
+
+
 export default PropertyList;
--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,28 +1,41 @@
+
+
+import { PropertyType } from "./PropertyList";
 import Image from "next/image";
 
 
-const PropertyListItem = () => {
+
+
+interface PropertyProps {
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
+}
+
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property,
+}) => {
     return (
         <div className="cursor-pointer">
-            <div className="relative overflow-hidden aspect-square rounded-xl">
+          <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
-                    src="/beach_1.jpg"
-                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                    src={property.image_url}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
             </div>
 
 
-           <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+            <div className="mt-2">
+                <p className="text-lg font-bold">{property.title}</p>
             </div>
 
 
             <div className="mt-2">
                 <p className="text-sm text-gray-700">
-                    <strong>$200</strong> per night
+                    <strong>{property.price_per_night}</strong> per night
                 </p>
             </div>
 
