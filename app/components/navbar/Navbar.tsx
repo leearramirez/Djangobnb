94b62ded09cfe9edf645eb0569daed3aa0c30d12
Leear <leear.ramirez@msunaawan.edu.ph>
2025-11-26 16:20:49 +0800

    Video 4 - Implement Authentication (Log out) - Leear Ramirez

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,40 +1,54 @@
 import Image from "next/image";
 import Link from "next/link";
 import SearchFilters from "./SearchFilters";
-import AddPropertyButton from "./AddPropertyButton";
 import UserNav from "./UserNav";
+import { getUserId } from "@/app/lib/action";
+import AddPropertyButton from "./AddPropertyButton";
 
-const Navbar = () => {
+
+const Navbar = async () => {
+    const userId = await getUserId();
+
+
+    console.log('userId:', userId);
     return (
-        <nav className="w-full fixed top-0 left-0 py-6 border-b border-gray-200 bg-white z-10">
+        <nav className="w-full fixed top-0 left-0 py-6 border-b border-gray-300 bg-white z-10">
             <div className="max-w-[1500px] mx-auto px-6">
                 <div className="flex justify-between items-center">
-                    
-                    {/* Logo */}
                     <Link href="/">
                         <Image
-                            src="/Logo.png"
+                            src="/logo.png"
                             alt="DjangoBnb logo"
                             width={180}
-                            height={38}
+                            height={30}
                         />
                     </Link>
 
-                    {/* Center Search Filters */}
+
                     <div className="flex space-x-6">
                         <SearchFilters />
                     </div>
 
+
                     <div className="flex items-center space-x-6">
-                        <AddPropertyButton />
+                        <AddPropertyButton
 
 
-                        <UserNav />
+                        />
+
+
+                        <UserNav
+                            userId={userId}
+                        />
                     </div>
                 </div>
             </div>
         </nav>
-    );
-};
+    )
+}
+
 
 export default Navbar;
+
+
+
