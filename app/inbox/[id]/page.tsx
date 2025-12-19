2878cc82ed4bab3fb7165583c775128f817b03bf
Lee Ar U. Ramirez <leear.ramirez@msunaawan.edu.ph>
2025-12-19 20:00:54 +0800

    video 7 / Send/Receive Messages - Lee Ar Ramirez

--- app/inbox/[id]/page.tsx
+++ app/inbox/[id]/page.tsx
@@ -1,7 +1,9 @@
-import { getUserId, getAccessToken } from "../../lib/action";
+import { getUserId } from "../../lib/action";
+import React from 'react';
 import apiService from "@/app/services/apiService";
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
 import { UserType } from "../page";
+import { getAccessToken } from "../../lib/action";
 
 
 export type MessageType = {
@@ -10,34 +12,39 @@ export type MessageType = {
     body: string;
     conversationId: string;
     sent_to: UserType;
-    created_by: UserType;
+    created_by: UserType
 }
 
-const ConversationPage = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const { id } = await params;
+
+const ConversationPage = async ({ params }: { params: { id: string } }) => {
     const userId = await getUserId();
     const token = await getAccessToken();
 
+
     if (!userId || !token) {
         return (
-            <main className="max-w-[1500px] mx-auto px-6 py-12">
+            <main className="max-w-[1500px] max-auto px-6 py-12">
                 <p>You need to be authenticated...</p>
             </main>
         )
     }
 
-    // I-pasa ang token para sa authentication
-    const conversation = await apiService.get(`/api/chat/${id}/`, token);
+
+    const { id } = await params;
+    const conversation = await apiService.get(`/api/chat/${id}/`, token)
+
 
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <ConversationDetail
                 token={token}
                 userId={userId}
+                messages={conversation.conversation.messages}
                 conversation={conversation.conversation}
             />
         </main>
     )
 }
+
 
 export default ConversationPage;
